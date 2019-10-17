c3bda7f6dfe5ac9dc5b6f3a43c21c2b8f18b6e56
Phil Swartz <pswartz.sju@gmail.com>
2019-10-16 21:44:07 -0400

    created a letter contructor class

---
+++ letter.js
@@ -0,0 +1,34 @@
+
+
+class letterConstructor {
+    constructor(guessLetter, isSearched) {
+
+        this.guessLetter = guessLetter;
+        this.isSearched = false;
+
+        this.returnCharacter = function() {
+
+
+        }
+
+        this.charCheck = function () {
+
+
+        }
+
+        this.printData = function () {
+
+            console.log("\n" + this.guessLetter +
+                        "\n" + this.isSearched);
+
+        }
+
+    }
+}
+
+
+var myletter = new letterConstructor('p', false);
+console.log(myletter);
+
+module.exports = letterConstructor;
+
